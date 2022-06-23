e3c14c45439dea8be093d51be9288c7193ee702f
Asha A <ashgirl23@hotmail.com>
2022-06-23 20:24:46 +0100

    first commit

---
+++ exercises.js
@@ -0,0 +1,150 @@
+
+// Exercises 1: Age logger
+let age = 33;
+console.log(age);
+
+// Exercise 2: So many rabbits
+let numberOfRabbits = 2;
+numberOfRabbits * 3;
+console.log(numberOfRabbits);
+
+// Exercises 3: BODMAS
+let bodmasExample = 3;
+answer = bodmasExample + 5;
+console.log(answer);
+result = answer / 6;
+console.log(result);
+
+// Exercises 4: Random number generator
+let randomNumber = Math.floor(Math.random() * 6 ) + 1;
+console.log(randomNumber);
+
+// String exercise 1: Log your country
+myCountry = "United Kingdom";
+console.log(myCountry);
+
+// String Exercise 2:
+myCountry.length;
+
+// String Exercise 3:
+let favouriteFood = "Asha";
+console.log(favouriteFood.toUpperCase());
+
+
+// String Exercise 4:
+console.log(favouriteFood.toLowerCase());
+
+
+// String Exercise 5:
+let myName = "Asha";
+let myAge = 33;
+let message ="Hello my name is ";
+console.log(message + myName);
+
+// String exercise 6:
+firstName = "Asha" ;
+lastName = " Ainte";
+console.log(firstName + lastName);
+let welcome = "Welcome "; 
+let question = "How can I help you?";
+console.log(welcome + firstName + lastName);
+console.log(question);
+
+// Control Flow Exercise 1: Password
+let password = 4;
+if (password  >7 ) 
+    console.log('true');
+ else 
+    console.log('false');
+
+// Control Flow Exercise 2: Height Test
+let height = 165;
+if (height >= 150 )
+console.log('You can go on the ride!');
+else
+console.log('Sorry you are not tall enough.');
+
+let input = 140;
+if (isNaN(input)){
+console.log('Please enter a number');
+} 
+if  (input >= 150 )
+console.log('You can ride');
+else { (input <150 )
+console.log('Sorry you are not tall enough ')
+
+}
+
+
+
+// if Statement Project: Dice Prize
+
+// A number between 1-6
+let diceOne = Math.floor(Math.random() * 6 ) + 1;
+let diceTwo = Math.floor(Math.random() * 6 ) + 1;
+console.log(diceOne) || console.log(diceTwo)
+
+
+
+if (diceOne === 6 && diceTwo === 6)
+console.log('you won a beaaaaarrr' ) 
+
+else
+
+if (diceOne === 1 && diceTwo === 1)
+console.log('you win 3 free throws' ) 
+
+else
+
+if (diceOne === 2 && diceTwo === 2)
+console.log('you win 1 free throw' ) 
+
+else
+
+
+if (diceOne === 3 && diceTwo === 3)
+console.log('you win 3 free throws' ) 
+
+else
+
+
+if (diceOne === 4 && diceTwo === 4)
+console.log('you win 1 free throw' ) 
+
+else
+
+
+if (diceOne === 5 && diceTwo === 5)
+console.log('you win 3 free throws' ) 
+
+else
+console.log('You lost')
+
+
+
+// Functions Exercise 1: Simple Function
+
+function greet () {
+
+    console.log('Hello there!');
+}
+
+greet();
+
+// Function Exercise 2: Quadrupler
+// I know this exercise doesn't work.
+function quadrupler (numb) {
+
+    quadrupler(5);
+    answerrr= quadrupler(5) * 4 ;
+    console.log(answerrr);
+}
+
+
+
+
+
+
+
+
+
